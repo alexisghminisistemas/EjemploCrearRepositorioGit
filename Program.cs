3f0e26cefff072bd65441b8371fb54b5b8f35ea9
Alexis Zahir García Hernández <alexis.garcia@minisistemas.mx>
2024-08-05 12:28:35 -0600

    Se agrego una operacion de suma al codigo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola mundo, esto es un pequeño curso de git!");
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine(num1 + num2);

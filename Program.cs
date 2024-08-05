f67e152a3e5ab840f6727d28a515b0b4a6a9ef5c
Alexis Zahir García Hernández <alexis.garcia@minisistemas.mx>
2024-08-05 17:39:31 -0600

    Add math basic operations

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola mundo, esto es un pequeño curso de git!");
+﻿
+Console.WriteLine("Hola mundo, esto es un pequeño curso de git!\n");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("El siguiente programa contiene operaciones aritméticas básicas");
+Console.WriteLine("*******************************************************************************");
 
 int num1 = 5;
 int num2 = 10;
 
-Console.WriteLine(num1 + num2);
+int sum = num1 + num2;
+int mult = num1 * num2;
+int res = num2 - num1;
+int div = num2 / num1;
 
-Console.WriteLine("Se creo la rama ejemplo_050824");
+Console.WriteLine("\nEl resultado de la suma es: " + sum);
+Console.WriteLine("El resultado de la resta es: " + res);
+Console.WriteLine("El resultado de la multiplicación es :" + mult);
+Console.WriteLine("El resultado de la division es :" + div);
